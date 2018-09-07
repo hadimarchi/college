132f4483e173ff3580945a8e7fcfd41005b9bae8
hadimarchi <hadimarchi@alaska.edu>
2018-09-06 19:36:55 -0800

    capstone hw1 with vector references instead of unique pointers to vectors

--- fall_2018/Capstone/HW1/algorithm.hpp
+++ fall_2018/Capstone/HW1/algorithm.hpp
@@ -8,24 +8,20 @@
 #include <memory>
 // std::unique_ptr
 // std::make_unique;
+#include <utility>
+//std::utility;
 
 namespace srt{
   using un_int = unsigned int;
 
   template <typename T>
-  using vec_ptr = std::unique_ptr<std::vector<T>>;
-
-  template <typename T>
   void print_vector(std::vector<T> const& vec_to_print);
 
   template <typename T>
-  void swap_at_vec_ptr(vec_ptr<T> & vec, un_int first_index, un_int second_index);
+  void quicksort(std::vector<T> &target_vec, un_int lower_index, un_int upper_index);
 
   template <typename T>
-  void quicksort(vec_ptr<T> &target_vec, un_int lower_index, un_int upper_index);
-
-  template <typename T>
-  un_int partition(vec_ptr<T> &target_vec, un_int lower_index, un_int upper_index);
+  un_int partition(std::vector<T> &target_vec, un_int lower_index, un_int upper_index);
 
   template <typename T>
   void do_quicksort(std::vector<T> &vec_to_sort);
@@ -41,14 +37,7 @@ void srt::print_vector(std::vector<T> const& vec_to_print){
 }
 
 template <typename T>
-void srt::swap_at_vec_ptr(vec_ptr<T> & vec, un_int first_index, un_int second_index){
-  auto temp = (*vec)[first_index];
-  (*vec)[first_index] = (*vec)[second_index];
-  (*vec)[second_index] = temp;
-}
-
-template <typename T>
-void srt::quicksort(vec_ptr<T> &target_vec, un_int lower_index, un_int upper_index){
+void srt::quicksort(std::vector<T> &target_vec, un_int lower_index, un_int upper_index){
   if (lower_index < upper_index){
     auto split_index = partition(target_vec, lower_index, upper_index);
     quicksort(target_vec, lower_index, std::max(split_index, un_int(1))-1);
@@ -57,18 +46,18 @@ void srt::quicksort(vec_ptr<T> &target_vec, un_int lower_index, un_int upper_ind
 }
 
 template <typename T>
-srt::un_int srt::partition(vec_ptr<T> &target_vec, un_int lower_index, un_int upper_index){
-  auto pivot = (*target_vec)[upper_index];
+srt::un_int srt::partition(std::vector<T> &target_vec, un_int lower_index, un_int upper_index){
+  auto pivot = target_vec[upper_index];
   auto split_index = lower_index - 1;
   for (auto i = lower_index; i<upper_index; i++){
-    if ((*target_vec)[i] < pivot){
+    if (target_vec[i] < pivot){
       split_index+=1;
-      swap_at_vec_ptr(target_vec, split_index, i);
+      std::swap(target_vec[split_index], target_vec[i]);
       }
   }
   split_index = split_index + 1;//std::min(split_index + 1, upper_index);
-  if ((*target_vec)[upper_index] < (*target_vec)[split_index]){
-    swap_at_vec_ptr(target_vec, split_index, upper_index);}
+  if (target_vec[upper_index] < target_vec[split_index]){
+    std::swap(target_vec[split_index], target_vec[upper_index]);}
 
   return split_index;
 }
@@ -77,9 +66,7 @@ srt::un_int srt::partition(vec_ptr<T> &target_vec, un_int lower_index, un_int up
 
 template <typename T>
 void srt::do_quicksort(std::vector<T> &vec_to_sort){
-  vec_ptr<T> vec_ptr = std::make_unique<std::vector<T>>(vec_to_sort);
-  srt::quicksort(vec_ptr, un_int(0), std::max(un_int((*vec_ptr).size()), un_int(1))-1);
-  print_vector(*vec_ptr);
+  srt::quicksort(vec_to_sort, un_int(0), std::max(un_int(vec_to_sort.size()), un_int(1))-1);
 
 }
 #endif
--- fall_2018/Capstone/HW1/test.cpp
+++ fall_2018/Capstone/HW1/test.cpp
@@ -1,8 +1,9 @@
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "algorithm.hpp"
 using srt::quicksort;
-using srt::swap_at_vec_ptr;
-using srt::vec_ptr;
+using srt::do_quicksort;
+using srt::print_vector;
 using srt::un_int;
 
 #include <vector>
@@ -11,8 +12,85 @@ using std::vector;
 using std::make_unique;
 #include <algorithm>
 using std::is_sorted;
+#include <stdlib.h>
+// rand();
 
-void test_trivial_lists(){
+vector<int> generate_random_list_of_integers(){
+  vector<int> randomly_generated = {};
+  for(un_int i=0; i<100; ++i){
+    randomly_generated.push_back((rand() % 200));
+  }
+  return randomly_generated;
+}
+TEST_CASE("Trivial lists are sorted"){
   vector<int> empty = {};
   vector<int> singleton = {1};
+  do_quicksort(empty);
+  do_quicksort(singleton);
+  REQUIRE(is_sorted(empty.begin(), empty.end()));
+  REQUIRE(is_sorted(singleton.begin(), singleton.end()));
+}
+
+TEST_CASE("Small lists are sorted"){
+  vector<int> pair = {2, 1};
+  vector<int> odd = {1,3,2};
+
+  do_quicksort(pair);
+  do_quicksort(odd);
+
+  REQUIRE(is_sorted(pair.begin(), pair.end()));
+  REQUIRE(is_sorted(odd.begin(), odd.end()));
+}
+
+TEST_CASE("Inconvenient lists are sorted"){
+  vector<int> right_min = {13, 25, 5,1};
+  vector<int> left_max = {100, 2, 1, 5};
+
+  do_quicksort(right_min);
+  do_quicksort(left_max);
+
+  REQUIRE(is_sorted(right_min.begin(), right_min.end()));
+  REQUIRE(is_sorted(left_max.begin(), left_max.end()));
+}
+
+TEST_CASE("Larger lists are sorted"){
+  vector<int> big = {22, 14, 3, 18, 4, 57, 13, 102, 0, 44};
+  vector<int> bigger = {132, 196, 38, 141, 121, 1, 152, 86, 184, 165, 156, 104,
+                        74, 88, 139, 194, 109, 64, 197, 56, 198, 186, 20, 100,
+                        171, 150, 142, 143, 31, 162, 80, 55, 126, 24, 23, 2,
+                        147, 65, 136, 40, 93, 82, 78, 29, 116, 119, 155, 48,
+                        182, 102, 69, 27, 188, 138, 43, 130, 45, 34, 90, 71,
+                        122, 30, 115, 7, 163, 108, 96, 129, 81, 25, 173, 178};
+
+  do_quicksort(big);
+  do_quicksort(bigger);
+
+  REQUIRE(is_sorted(big.begin(), big.end()));
+  REQUIRE(is_sorted(bigger.begin(), bigger.end()));
+
+}
+
+TEST_CASE("Lists with negative integers are sorted"){
+  vector<int> negatives = {-5, -11, -1, -2};
+  vector<int> mixed = {11, -49, 2, -2};
+
+  do_quicksort(negatives);
+  do_quicksort(mixed);
+
+  REQUIRE(is_sorted(negatives.begin(), negatives.end()));
+  REQUIRE(is_sorted(mixed.begin(), mixed.end()));
+}
+
+TEST_CASE("Character list is sorted"){
+  vector<char> string = {'y', 't', 'a', 'f', 'r'};
+  do_quicksort(string);
+  REQUIRE(is_sorted(string.begin(), string.end()));
+}
+
+TEST_CASE("Random list is sorted"){
+  auto randomly_generated = generate_random_list_of_integers();
+
+  do_quicksort(randomly_generated);
+  REQUIRE(is_sorted(randomly_generated.begin(), randomly_generated.end()));
+
 }
